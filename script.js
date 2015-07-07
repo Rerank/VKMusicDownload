0993be88d1c055290516f49a80e2dc2b39458f9d
Rerank <bip78z@gmail.com>
2015-07-07 20:23:15 +0300

    Теперь работает пре переходах по страницам

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
 $(function(){
+var listObserver = new MutationObserver(newDOM); //при изменении DOM вызов функции newDOM
+listObserver.observe(document.body, {childList: true, subtree: true}); //слушаем body
 
-	alert('Версия1 jQuery ' + jQuery.fn.jquery);
-	$('.audio').each(function (index) {
-	var title = $(this).find('.title_wrap').find("a").text();
-
-	var $play_btn = $(this).find('.play_btn')
-	if($play_btn.length)
+		//alert('Версия1 jQuery ' + jQuery.fn.jquery);
+		$('.audio').each(function (index) {
+		processingOfAudio(this);
+		});
+	
+	function processingOfAudio(current)
 	{
-	  var directLink = $play_btn.find("input:hidden").val();
-	}
-	else
-	{
-	  var directLink = $(this).find('.play_btn_wrap').siblings('input').val();
+		var $download_btn = $(current).find('.download_btn');
+		if (!$download_btn.length) { //проверка, нет ли такой кнопки уже на элементе
+
+			var title = $(current).find('.title_wrap').find("a").text();
+
+			var $play_btn = $(current).find('.play_btn')
+			if($play_btn.length) // такой элемент на странице аудиозаписей
+			{
+			  var directLink = $play_btn.find("input:hidden").val();
+			}
+			else // а такой в постах
+			{
+			  var directLink = $(current).find('.play_btn_wrap').siblings('input').val();
+
+			}
+
+			var $titleSpan = $(current).find('.title_wrap').find("span.title"); // передается в функцию ниже, после него будет вставляться кнопка
+
+			var $area = $(current).find('.area');
+			$area.hover(
+			  function() {
+			    $area.find(".download_btn").show();
+			  }, function() {
+			    $area.find(".download_btn").hide();
+			  }
+			);
+			appendBtn($titleSpan, directLink, title);			
+		};
+
 
 	}
-	var $titleSpan = $(this).find('.title_wrap').find("span.title");
-	var $area = $(this).find('.area');
-	$area.hover(
-	  function() {
-	    $area.find(".download_btn").show();
-	  }, function() {
-	    $area.find(".download_btn").hide();
-	  }
-	);
-	appendBtn($titleSpan, directLink, title);
-	});
 
+	function newDOM(mutations)
+    {
+   		for (var i = 0; i < mutations.length; i++)
+        {
+            var added = mutations[i].addedNodes;
+
+            findAudio(added);
+            
+        }
+	}
+
+	function findAudio(added)
+    {
+    	var $audio = $(added).find('.audio');
+    	
+    	if ($audio.length) {
+    		$audio.each(function (index) {
+    			processingOfAudio(this);
+    			});
+    	};
+    }
 
 	function appendBtn(titleSpan, directLink, title) {
 	  var btn = document.createElement('a');
@@ -45,3 +81,7 @@ $(function(){
 	titleSpan.after(btn);
 	}
 });
+
+
+
+	
