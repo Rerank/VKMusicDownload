7e5a14c98408056224b3789507396abf804e676f
Roman Shestilovski <emne@null.net>
2015-07-09 15:06:06 +0300

    Clean up.

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
-if (request.command == 'download') {
-	var url = request.params.url;
-	var filename = request.params.filename;
-	chrome.downloads.download({
-		url: url,
-		filename: filename 
-	});
-};
-
+	if(request.command == 'download') {
+		var url = request.params.url;
+		var filename = request.params.filename;
+		chrome.downloads.download({
+			url: url,
+			filename: filename 
+		});
+	}
+	
 });
